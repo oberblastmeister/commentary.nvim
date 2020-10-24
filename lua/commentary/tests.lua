bbefa5a78756daf1fdc08d5a83aab07163f64ede
Brian Shu <littlebubu.shu@gmail.com>
2020-10-23 21:26:37 -0400

    fnished refractoring

--- lua/commentary/tests.lua
+++ lua/commentary/tests.lua
@@ -1,1 +1,114 @@
+require 'busted.runner'()
+
 local utils = require("utils")
+
+describe("#string", function()
+  it("escape lua pattern", function()
+    assert.are.equals(string.escape_lua_pattern("/*"), "/%*")
+    assert.are.equals(string.escape_lua_pattern("**/"), "%*%*/")
+    assert.are.equals(string.escape_lua_pattern("?hello*[hello]"), "%?hello%*%[hello%]")
+  end)
+end)
+
+describe("#insert", function()
+  it("simple", function()
+    assert.are.equals(string.insert("hello", " dude ", 2), "he dude llo")
+  end)
+
+  it("beginning", function()
+    assert.are.equals(string.insert("person", "wow ", 0), "wow person")
+  end)
+
+  it("one", function()
+    assert.are.equals(string.insert("person", " wow ", 1), "p wow erson")
+  end)
+
+  it("end", function()
+    local s = "person"
+    assert.are.equals(string.insert(s, " wow", s:len()), "person wow")
+  end)
+
+  it("whitespace", function()
+    assert.are.equals(string.insert("hi", " ", 0), " hi")
+  end)
+
+  it("whitespace end", function()
+    assert.are.equals(string.insert("hi", " ", 0), " hi")
+  end)
+end)
+
+describe("#white space", function()
+  it("add back", function()
+    assert.are.equals(string.add_whitespace("hi", true, false), " hi")
+  end)
+
+  it("add front", function()
+    assert.are.equals(string.add_whitespace("hi", false, true), "hi ")
+  end)
+
+  it("add front", function()
+    assert.are.equals(string.add_whitespace("hi", true, true), " hi ")
+  end)
+
+  it("trim whitespace front", function()
+    assert.are.equals(string.trim1("  hi", true, false), " hi")
+  end)
+
+  it("trim whitespace back", function()
+    assert.are.equals(string.trim1(" hi ", false, true), " hi")
+  end)
+
+  it("trim whitespace both", function()
+    assert.are.equals(string.trim1(" hi  ", true, true), "hi ")
+  end)
+end)
+
+describe("#comment type", function()
+  it("c", function()
+    assert.is_true(utils.is_surround_comment("/*%s*/"))
+  end)
+
+  it("python", function()
+    assert.is_not_true(utils.is_surround_comment("#%s"))
+  end)
+
+  it("rust", function()
+    assert.is_not_true(utils.is_surround_comment("//%s"))
+  end)
+end)
+
+describe("#comment", function()
+  it("c", function()
+    assert.are.equals(utils.comment("this is a line", "/*%s*/"), "/* this is a line */")
+  end)
+
+  it("rust", function()
+    assert.are.equals(utils.comment("another line", "//%s"), "// another line")
+  end)
+
+  it("python", function()
+    assert.are.equals(utils.comment("python code", "#%s"), "# python code")
+  end)
+
+  it("leading whitespace c", function()
+    assert.are.equals(utils.comment(" leading whitespace", "/*%s*/"), "/*  leading whitespace */")
+  end)
+end)
+
+describe("#uncomment", function()
+  it("c", function()
+    assert.are.equals(utils.uncomment("/* commented */", "/*%s*/"), "commented")
+    assert.are.equals(utils.uncomment("/* this is commented */", "/*%s*/"), "this is commented")
+    assert.are.equals(utils.uncomment("/*/* this is commented */", "/*%s*/"), "/* this is commented")
+    assert.are.equals(utils.uncomment("/*/* this is commented */*/", "/*%s*/"), "/* this is commented */")
+    assert.are.equals(utils.uncomment("this is commented */*/", "/*%s*/"), "this is commented */*/")
+  end)
+
+  it("python", function()
+    assert.are.equals(utils.uncomment("# this is commented", "#%s"), "this is commented")
+  end)
+
+  it("multiple comments", function()
+    assert.are.equals(utils.uncomment("# /* this is commented */", "#%s"), "/* this is commented */")
+  end)
+end)
--- lua/commentary/utils.lua
+++ lua/commentary/utils.lua
@@ -1,36 +1,99 @@
 local M = {}
 
-function M.insert(str1, str2, pos)
-    local sub1 = string.sub(str1, 1, pos)
-    local sub2 = string.sub(str1, pos + 1)
-    return sub1 .. str2 .. sub2
+function string.debug(s)
+  print(string.format("'%s'", s))
 end
 
-function M.add_white_space(str, ...)
-  local opts = {...}
-  if opts[0] then
-    str = insert(str, " ", 0)
+-- trim one space from front, back, or both
+function string.trim1(s, front, back)
+  if front then s = s:gsub("^%s", "") end
+  if back then s = s:gsub("%s$", "") end
+  return s
+end
+
+do
+  local matches =
+  {
+    ["^"] = "%^";
+    ["$"] = "%$";
+    ["("] = "%(";
+    [")"] = "%)";
+    ["%"] = "%%";
+    ["."] = "%.";
+    ["["] = "%[";
+    ["]"] = "%]";
+    ["*"] = "%*";
+    ["+"] = "%+";
+    ["-"] = "%-";
+    ["?"] = "%?";
+    ["\0"] = "%z";
+  }
+
+  -- escape all patterns in lua string to be able to match literal string
+  function string.escape_lua_pattern(s)
+    return s:gsub(".", matches)
   end
-  if opts[1] then
-    str =  insert(str, " ", str:len())
-  end
+end
+
+-- add whitespace at the front, end, or both
+function string.add_whitespace(str, front, back)
+  if front then str = string.insert(str, " ", 0) end
+  if back then str = string.insert(str, " ", str:len()) end
   return str
 end
 
--- comment string defined as 
-function M.comment(line, commentstring)
-  if string.match(commentstring, ".+%%s.+") then
-    line = add_white_space(line, true, true)
+-- Insert str2 into str1 at index. 0 means prepend. 1 means insert after the first character (1 indexed).
+-- Inserting equal to the lenght of the string means appending to the string
+function string.insert(str1, str2, pos)
+  local sub1 = string.sub(str1, 1, pos)
+  local sub2 = string.sub(str1, pos + 1)
+  return sub1 .. str2 .. sub2
+end
+
+-- wheather the comment is a surround comment (c style) comment or not (python style)
+function M.is_surround_comment(commentstring)
+  if commentstring:find(".+%%s.+") then return true else return false end
+end
+
+function M.get_comment_part(commentstring)
+  if M.is_surround_comment(commentstring) then
+    return commentstring:match("(.+)%%s(.+)")
   else
-    line = add_white_space(line, true)
+    return commentstring:match("(.+)%%s")
+  end
+end
+
+-- comment string defined as
+function M.comment(line, commentstring)
+  if M.is_surround_comment(commentstring) then
+    line = string.add_whitespace(line, true, true)
+  else
+    line = string.add_whitespace(line, true, false)
   end
 
   return string.format(commentstring, line)
 end
 
 function M.uncomment(line, commentstring)
-  local comment_part = commentstring:gsub("%%s", "")
-  line:gsub(comment_part .. "%s*", "")
+  local cmnt1, cmnt2 = M.get_comment_part(commentstring)
+  cmnt1 = string.escape_lua_pattern(cmnt1)
+  if cmnt2 then cmnt2 = string.escape_lua_pattern(cmnt2) end
+
+  local pattern
+  if cmnt2 then
+    local pattern1 = cmnt1 .. "%s?"
+    local pattern2 = "%s?" .. cmnt2
+    local full_pattern = pattern1 .. "(.*)" .. pattern2
+    print('full pattern:')
+    string.debug(full_pattern)
+    line = line:gsub(full_pattern, "%1")
+    print('line:')
+    string.debug(line)
+    return line
+  else
+    pattern = string.escape_lua_pattern(cmnt1) .. "%s?"
+    line = line:gsub(pattern, "", 1)
+  end
   return line
 end
 
@@ -50,3 +113,5 @@ function M.toggle_comment(line, commentstring)
     return uncomment(line, commentstring)
   end
 end
+
+return M
